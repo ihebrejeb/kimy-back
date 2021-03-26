c1fadfed98137b7a67194941669191e707ca1dca
Iheb Rejeb <iheb.rejeb@esprit.tn>
2021-03-26 20:09:02 +0100

    added deleteroom endpoint

--- routers/roomsRouter.js
+++ routers/roomsRouter.js
@@ -2,14 +2,17 @@ const express = require("express");
 const router = express.Router();
 const room = require("../models/Room");
 
-router.route("/").post((req, res) => {
+router.route("/").post(async (req, res) => {
   const { roomSID, course } = req.body;
-  room.create({ roomSID, course });
+  const doc = await room.findOne({ roomSID: roomSID });
+  if (!doc) await room.create({ roomSID, course });
   res.json("done");
 });
 router.route("/updateStatus/:roomId").post(async (req, res) => {
   const { roomId } = req.params;
-  await room.findOneAndUpdate({ roomSID: roomId }, { recordingStatus: true });
+  const doc = await room.findOne({ roomSID: roomId });
+  doc.recordingStatus = true;
+  await doc.save();
   res.json("done");
 });
 router.route("/:courseid").get(async (req, res) => {
@@ -17,5 +20,9 @@ router.route("/:courseid").get(async (req, res) => {
   const data = await room.find({ course: courseid });
   res.json(data);
 });
-
+router.route("/:roomSID").delete(async (req, res) => {
+  const { roomSID } = req.params;
+  const data = await room.deleteOne({ roomSID });
+  res.json(data);
+});
 module.exports = router;
