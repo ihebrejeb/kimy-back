c0327d04a5ab4382db30bf0a0367133d2e4bc8e0
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-04-17 00:39:37 +0000

    adding search

--- routers/CourseActivityRouter.js
+++ routers/CourseActivityRouter.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 const Activities = require("../controllers/CourseActivityController");
+const activity = require("../models/CourseActivityModel");
 
 router
   .route("/")
@@ -14,11 +15,10 @@ router
   .patch(Activities.UpdateActivity)
   .get(Activities.GetActivity);
 
-  
-router.route("/search/:search").get(async (req, res) => {
+router.route("/search/:search").get(async (req, res, next) => {
   try {
     const { search } = req.params;
-    const doc = await forums.find({
+    const doc = await activity.find({
       $or: [{ title: { $regex: search, $options: "i" } }],
     });
     res.status(200).json(doc);
