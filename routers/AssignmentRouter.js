d5b6e1b2e913647881ee776505f96fab20fff12f
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-04-21 22:21:44 +0000

    Update AssignmentRouter.js

--- routers/AssignmentRouter.js
+++ routers/AssignmentRouter.js
@@ -2,7 +2,24 @@ var express = require("express");
 var router = express.Router();
 var assignment = require("../models/AssignmentModel");
 const Assignments = require("../controllers/AssignmentController");
+router.route("/sortAsc").get(async (req, res, next) => {
+  try {
+    const doc = await assignment.find().sort({ dateLimite: 1 });
 
+    res.status(200).json(doc);
+  } catch (error) {
+    next(error);
+  }
+});
+router.route("/sortDesc").get(async (req, res, next) => {
+  try {
+    const doc = await assignment.find().sort({ dateLimite: -1 });
+
+    res.status(200).json(doc);
+  } catch (error) {
+    next(error);
+  }
+});
 router
   .route("/")
   .get(Assignments.GetAllAssignments)
