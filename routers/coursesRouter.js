5996447d951096a8e72b331e94db2656398f36bb
medhabibdridi <48842413+medhabibdridi@users.noreply.github.com>
2021-05-01 02:58:28 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/ihebrejeb/kimy-back into main""
    This reverts commit a233e023515b229a801788aab69f3f51c50da508.

--- routers/coursesRouter.js
+++ routers/coursesRouter.js
@@ -3,9 +3,11 @@ var router = express.Router();
 const authController = require("../controllers/authController");
 
 const coursesController = require("../controllers/coursesController");
-router.route("/search/:search").get(coursesController.GetCode)
 
+router.route("/search/:search").get(coursesController.GetCode)
 router.use(authController.protect);
+router.route("/enroll/:id").post(coursesController.enrollStudent)
+
 
 router
   .route("/")
